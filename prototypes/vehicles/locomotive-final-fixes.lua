1decaa11b32e9e1358c800e815eff6661083f4d3
starkita <maverick.arrows@gmail.com>
2020-04-03 13:50:20 +0200

    Rimosso data.lua, sistemata indentazione, aggiornata mod homepage

--- prototypes/vehicles/locomotive-final-fixes.lua
+++ prototypes/vehicles/locomotive-final-fixes.lua
@@ -1,2 +1,2 @@
-	data.raw.locomotive["locomotive"].max_power = "900KW"
-  data.raw.locomotive["locomotive"].max_speed = 1.8
+    data.raw.locomotive["locomotive"].max_power = "900KW"
+    data.raw.locomotive["locomotive"].max_speed = 1.8
