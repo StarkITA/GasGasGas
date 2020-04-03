b096b87829a3401d689d5b1c3bce71161f33a62d
starkita <maverick.arrows@gmail.com>
2020-04-04 00:02:56 +0200

    0.0.3
    Increased power consumption + added changelog

--- prototypes/vehicles/car-final-fixes.lua
+++ prototypes/vehicles/car-final-fixes.lua
@@ -3,4 +3,4 @@
 
     data.raw.car["car"].max_health = 1000
     data.raw.car["car"].inventory_size = 140
-    data.raw.car["car"].consumption = "250KW"
+    data.raw.car["car"].consumption = "500KW"
--- prototypes/vehicles/locomotive-final-fixes.lua
+++ prototypes/vehicles/locomotive-final-fixes.lua
@@ -1,2 +1,2 @@
-    data.raw.locomotive["locomotive"].max_power = "900KW"
+    data.raw.locomotive["locomotive"].max_power = "1.8MW"
     data.raw.locomotive["locomotive"].max_speed = 1.8
